a211f25e92f143f1acd51ef16aa12438b1d3cc0a
Akshay Nair <hello@akshaynair.com>
2017-02-11 03:12:07 -0500

    Add physics

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,4 +32,73 @@ $(document).ready(function() {
     });
 
 
+    Physics(function(world) {
+        world.add(Physics.integrator('verlet'));
+        var renderer = Physics.renderer('canvas', {
+            el: 'viewport',
+            meta: false, // don't display meta data
+            styles: {
+                // set colors for the circle bodies
+                'circle': {
+                    strokeStyle: '#351024',
+                    lineWidth: 1,
+                    fillStyle: '#b7b2af'
+                },
+                // set colors for the circle bodies
+                'rectangle': {
+                    strokeStyle: '#FFFFFF',
+                    lineWidth: 1,
+                    fillStyle: '#ADD8FF'
+                }
+            }
+        });
+
+        // add the renderer
+        world.add(renderer);
+        // render on each step
+        world.on('step', function() {
+            world.render();
+        });
+        // bounds of the window
+        var viewportBounds = Physics.aabb(0, 0, 2000, 500);
+
+        // constrain objects to these bounds
+        world.add(Physics.behavior('edge-collision-detection', {
+            aabb: viewportBounds,
+            restitution: 0.99,
+            cof: 0.99
+        }));
+
+        world.add(Physics.body('circle', {
+            x: 250, // x-coordinate
+            y: 100, // y-coordinate
+            radius: 20,
+            restitution: 0.2
+        }));
+        world.add(Physics.body('circle', {
+            x: 50, // x-coordinate
+            y: 100, // y-coordinate
+            radius: 20,
+            restitution: 0.8
+        }));
+        world.add(Physics.body('rectangle', {
+            x: 250, // x-coordinate
+            y: 500, // y-coordinate
+            width: 200,
+            height: 10,
+            restitution: 0
+        }));
+        world.add(Physics.behavior('body-impulse-response'));
+        world.add(Physics.behavior('constant-acceleration'));
+        // subscribe to the ticker
+        Physics.util.ticker.on(function(time) {
+            world.step(time);
+        });
+        // start the ticker
+        Physics.util.ticker.start();
+    });
+
+
+
+
 });
